23a497f73afd683d67e9f57ac9f92bbd51de062b
anujteotia <anujteotia06@gmail.com>
2017-09-14 17:20:28 +0530

    First commit

---
+++ src/com/start/Hello.java
@@ -0,0 +1,14 @@
+package com.start;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Hello {
+	public final static Logger logger = LogManager.getLogger(Hello.class);
+	public static void main(String[] args) {
+		logger.info("Hello World!!!");
+		logger.info("Naacho!!!!!");
+	}
+
+}
+	
