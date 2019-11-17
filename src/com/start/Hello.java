4a68fcb6ca893ffc998eb69c904a871701ac1e4d
anujteotia <31957650+anujteotia@users.noreply.github.com>
2019-11-17 12:39:06 +0100

    Update Hello.java

--- src/com/start/Hello.java
+++ src/com/start/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	public final static Logger logger = LogManager.getLogger(Hello.class);
 	public static void main(String[] args) {
 		logger.info("Hello World!!!");
-		logger.info("Naacho!!!!!");
+		logger.info("Let's begin!!!!!");
 	}
 
 }
